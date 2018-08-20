919326b6ec6964b300f954bc740b0c1b48e4bac9
Vladimirtsarev <svarka2004@inbox.ru>
2018-08-20 18:11:47 +0300

    _

--- csharp2_wpf/MainWindow.xaml.cs
+++ csharp2_wpf/MainWindow.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace csharp2_wpf
 {
+    public class Employee { }
+    public class Department { }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Department> _dep = new List<Department>();
+
         public MainWindow()
         {
             InitializeComponent();
+            ReadFromFile();
         }
+
+        private void ReadFromFile()
+        {
+            
+           string[] s= File.ReadAllText("Departments.txt").Split('\n');
+            foreach  (string str in s)
+            {
+                comboBox1.Items.Add(str.Replace("\r",""));
+            }
+        }
+        
     }
 }
